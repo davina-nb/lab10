74ee149cb7fa4e5b0eeb21203194ba04c6736ac0
davina-nb <dnb24@njit.edu>
2024-11-26 22:57:01 -0500

    completed lab

--- script.js
+++ script.js
@@ -7,5 +7,13 @@ greetUser('Davina');
 function button(){
     document.getElementById("message").innerText = "Hello world!";
 } 
-button();
 
+function AddNumbers(){
+    let a = 10;
+    let b = 100;
+    console.log(a+b);
+}
+function button1(){
+    document.getElementById("btn1").value= AddNumbers();
+    document.getElementById("result").innertext = AddNumbers();
+}
